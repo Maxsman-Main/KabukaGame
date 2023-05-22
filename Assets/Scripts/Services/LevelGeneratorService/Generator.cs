e6ac8cf1a8b6b203bded2684969f5f6bad435f20
Maxsman <dekxirus@gmail.com>
2023-05-22 16:18:36 +1000

    Generation formula was added

--- Assets/Scripts/Services/LevelGeneratorService/Generator.cs
+++ Assets/Scripts/Services/LevelGeneratorService/Generator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Services.ResourceLoaderService;
 using UnityEngine;
@@ -23,22 +24,65 @@ namespace Services.LevelGeneratorService
         public List<GameObject> GenerateRandomLevel(int size)
         {
             GameObject[] levels = _resourceLoader.LoadPrefabs(LevelsPath);
+            var generatedLevels = new List<GameObject>();
             var levelsList = levels.ToList();
-            List<GameObject> shuffledList = ShuffleList(levelsList);
-            shuffledList = shuffledList.GetRange(0, size);
-            return shuffledList;
-        }
-
-        private List<GameObject> ShuffleList(List<GameObject> values)
-        {
-            var rand = new Random();
+            var difficultList = MakeDifficultList(levelsList);
+            var random = new Random();
             
-            for (var i = values.Count - 1; i > 0; i--) {
-                var k = rand.Next(i + 1);
-                (values[k], values[i]) = (values[i], values[k]);
+            for (int i = 0; i < size; i++)
+            {
+                var probabilities = GetProbabilities(difficultList, i + 1, 0.1);
+                var next = random.NextDouble();
+                for (var index = 0; index < probabilities.Count; index++)
+                {
+                    var probability = probabilities[index];
+                    if (!(probability[0] <= next) || !(next < probability[1])) continue;
+                    generatedLevels.Add(levelsList[index]);
+                    levelsList.RemoveAt(index);
+                    difficultList.RemoveAt(index);
+                }
             }
 
-            return values;
+            return generatedLevels;
+        }
+
+        private List<int> MakeDifficultList(List<GameObject> levelPatterns)
+        {
+            return levelPatterns
+                .Select(pattern => 
+                        pattern.GetComponent<LevelPattern>())
+                    .Select(levelPattern => levelPattern.LevelComplexity)
+                    .ToList();
+        }
+
+        private double GetWeight(int complexity, int weightNumber, double multiplier)
+        {
+            return complexity * Math.Exp(-complexity * multiplier * weightNumber);
+        }
+
+        private List<List<double>> GetProbabilities(List<int> difficulties, int generateStep, double multiplier)
+        {
+            var probabilities = new List<List<double>>();
+            var weights = new List<double>();
+            var total = 0d;
+            foreach (var difficult in difficulties)
+            {
+                var weight = GetWeight(difficult, generateStep, multiplier);
+                total += weight;
+                weights.Add(weight);
+            }
+
+            var right = 0d;
+            foreach (var weight in weights)
+            {
+                var left = right;
+                right += weight;
+                probabilities.Add(new List<double> 
+                    {left / total, right / total}
+                );
+            }
+
+            return probabilities;
         }
     }
 }
--- Assets/Scripts/Services/LevelGeneratorService/LevelPattern.cs
+++ Assets/Scripts/Services/LevelGeneratorService/LevelPattern.cs
@@ -9,10 +9,13 @@ namespace Services.LevelGeneratorService
         [SerializeField] private StartPoint playerPosition;
         [SerializeField] private EndPoint patternEndPosition;
         [SerializeField] private List<SpawnGroup> spawnGroups;
+        [SerializeField] private int levelComplexity;
 
         public Transform PlayerPosition => playerPosition.transform;
         public Transform PatternEndPosition => patternEndPosition.transform;
 
+        public int LevelComplexity => levelComplexity;
+        
         public void MakePatternGeneration()
         {
             foreach (var spawnGroup in spawnGroups)
