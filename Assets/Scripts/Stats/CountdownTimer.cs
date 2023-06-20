d5381bafb0f81cc36a09d67c1dece923f660e9fb
Brentimus <Gamepopins@gmail.com>
2023-06-20 22:30:54 +1000

    Add timer

---
+++ Assets/Scripts/Stats/CountdownTimer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    public float duration = 60.0f;
+    public Action onTimerExpired;
+
+    private Text timerText;
+
+    private float currentTime;
+
+    private void Start()
+    {
+        StartTimer();
+        timerText = gameObject.GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        if (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+            if (currentTime <= 0)
+            {
+                currentTime = 0;
+                onTimerExpired?.Invoke();
+            }
+        }
+        UpdateTimerText();
+    }
+
+    public void StartTimer()
+    {
+        currentTime = duration;
+    }
+    private void UpdateTimerText()
+    {
+        if (timerText != null)
+        {
+            int minutes = Mathf.FloorToInt(currentTime / 60);
+            int seconds = Mathf.FloorToInt(currentTime % 60);
+            timerText.text = string.Format("{0}:{1:00}", minutes, seconds);
+        }
+    }
+}
