6eea05065c4bc7e56e7749bbee3799c4676f50de
Maxsman <dekxirus@gmail.com>
2024-04-18 17:31:09 +1000

    Fix bug with damge provider

--- Assets/Scripts/LevelObjects/DamageProvider.cs
+++ Assets/Scripts/LevelObjects/DamageProvider.cs
@@ -6,14 +6,27 @@ using Zenject;
 public class DamageProvider : MonoBehaviour
 {
     [SerializeField] private int amount = 1;
-    [Inject] private LevelController _levelController;
-
+    
+    private GameObject _player;
+    
+    private void Start()
+    {
+        _player = GameObject.FindWithTag("Player");
+    }
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Health.Instance.GetDamage(amount);
-            _levelController.TeleportPlayerOnPatternStart(FindObjectOfType<StartPoint>().transform);
+            TeleportPlayerOnPatternStart(FindObjectOfType<StartPoint>().transform);
         }
+    }
+    
+    private void TeleportPlayerOnPatternStart(Transform point)
+    {
+
+        _player.gameObject.transform.position = point.position;
+        _player.GetComponent<Rigidbody>().velocity = Vector3.zero;
     }
 }
